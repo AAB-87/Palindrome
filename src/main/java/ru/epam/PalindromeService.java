00c56b8c3a8bbce7973e148acb17531e1a7962db
AAB-87 <andrej-abrekov@yandex.ru>
2023-09-09 15:31:18 +0400

    updated the conditions

--- src/main/java/ru/epam/PalindromeService.java
+++ src/main/java/ru/epam/PalindromeService.java
@@ -6,22 +6,24 @@ import java.util.Scanner;
 public class PalindromeService {
 
     public static void main(String[] args) {
-        Scanner inputScanner = new Scanner(System.in);
-        System.out.print("Введите пример: ");
-        String input = inputScanner.nextLine();
-        Scanner in = new Scanner(System.in);
-        String name = input.replaceAll("\\s+", "").toLowerCase();
-        int length = name.length();
-        int forward = 0;
-        int backward = length - 1;
-        while (backward >= forward) {
-            char forwardChar = name.charAt(forward++);
-            char backwardChar = name.charAt(backward--);
-            if (forwardChar != backwardChar) {
-                System.out.println("Пример: " + input + " - не палиндром");
-            }
+        char forwardChar = 0;
+        char backwardChar = 0;
+        Scanner inputScanner = new Scanner(System.in); // Объявление и инициализация переменной типа Scanner
+        System.out.print("Введите пример: "); // Выводим предложение пользователю
+        String input = inputScanner.nextLine(); // Сканируем введённый пример
+        String example = input.replaceAll("\\s+", "").toLowerCase(); // Убираем пробелы между словами и трансформируем буквы в строчные, где \\s+ - соответствует последовательности из одного или нескольких пробельных символов меняется на "" - пустая строка
+        int length = example.length(); // Определяем кол-во букв в ведённом примере
+        int forward = 0; // Создаём переменную со значением 0, для начала отсчёта перебора
+        int backward = length - 1; // Создаём переменную в которой будет храниться кол-во букв в ведённом примере - 1
+        while (backward > forward) { // пока кол-во букв -1, больше 0
+            forwardChar = example.charAt(forward++);
+            backwardChar = example.charAt(backward--);// Из введённого примера без пробелов вызываем метод, который разбивает каждую букву по индексам и минусует 1
+        }
+        if (forwardChar != backwardChar) {
+            System.out.println("Пример: " + input + " - не палиндром");
+        } else {
             System.out.println("Пример: " + input + " - палиндром");
         }
     }
-
 }
+
